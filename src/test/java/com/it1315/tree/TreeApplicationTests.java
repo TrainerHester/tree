768ef6e568a3940a6d9cfe5870cf8f056da8ae1b
PC201504192016\Administrator <hesternanweiguo@gmail.com>
2018-10-16 13:21:13 +0800

    add service test

--- src/test/java/com/it1315/tree/TreeApplicationTests.java
+++ src/test/java/com/it1315/tree/TreeApplicationTests.java
@@ -1,16 +1,26 @@
 package com.it1315.tree;
 
+import com.it1315.tree.entity.Tree;
+import com.it1315.tree.service.TreeService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TreeApplicationTests {
 
+    @Autowired
+    TreeService treeService;
+
     @Test
     public void contextLoads() {
+        List<Tree> trees = treeService.findTreeById(2);
+        System.out.println(trees.get(0));
     }
 
 }
