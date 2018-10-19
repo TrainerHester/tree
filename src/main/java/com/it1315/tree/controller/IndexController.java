75f47f447c529e98844625557f09f6410a49141b
PC201504192016\Administrator <hesternanweiguo@gmail.com>
2018-10-19 14:43:13 +0800

    add front end code

---
+++ src/main/java/com/it1315/tree/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.it1315.tree.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dly on 2018/10/19
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "/index.html";
+    }
+
+}
